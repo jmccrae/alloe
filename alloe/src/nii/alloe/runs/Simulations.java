33f85faaca42abccd65a5e56075413e9deb920a9
johnmccrae <johnmccrae@da035120-5443-0410-935a-9f624c77cda8>
2008-03-07 02:37:12 +0000

    ""

--- alloe/src/nii/alloe/runs/Simulations.java
+++ alloe/src/nii/alloe/runs/Simulations.java
@@ -33,19 +33,17 @@ public class Simulations {
      */
     public static void main(String[] args) {
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("results"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("results-" + args[1]));
             bw.write("connect,time_complete,fm_pre,fm_post_complete,rows,cols,cm1,cm2,cm3,cm4,cm5,cm6\n");
-            for(double d = -1; d <= -0.6; d += 0.01) {
+            for(int n = 50; n < 4000; n += 50) {
                 double[] res = new double[VEC];
-                double[] resSq = new double[VEC];
-                for(int i = 0; i < 500; i++) {
-                    double[] res2 = doRun(.8,.8,100,"logics/synonym.logic",d,1);
+                for(int i = 0; i < 1; i++) {
+                    double[] res2 = doRun(.8,.8,n,args[0],-.8,.15);
                     for(int j = 0; j < VEC; j++) {
                         res[j] += res2[j];
-                        resSq[j] += res2[j] * 2;
                     }
                 }
-                bw.write(d + "," + Strings.join(",",res) +","+ Strings.join(",",resSq) + "\n");
+                bw.write(n + "," + Strings.join(",",res) + "\n");
                 bw.flush();
             }
             bw.close();
@@ -129,94 +127,5 @@ public class Simulations {
         return null;
     }
     
-    public static double[] jointImprovement(double prec, double recall, int n, double linkDensity, double sparsity) {
-        try {
-            Simulate s = new Simulate("logics/sh.logic",prec,recall,n);
-            Iterator<String> relIter = s.l.relationDensity.keySet().iterator();
-            while(relIter.hasNext()) {
-                s.l.relationDensity.put(relIter.next(),linkDensity);
-            }
-            s.sparsePercent = sparsity;
-            s.createModels();
-            
-            double []rval = new double[18];
-            int []tempRes = new int[3];
-            
-            TreeMap<String,Graph> graphs = new TreeMap<String,Graph>();
-            graphs.put("e",s.probModel.getGraphByName("e"));
-            graphs.put("r1",s.probModel.getGraphByName("r2"));
-            Model synOnlyProbModel = new Model(graphs,n);
-            graphs = new TreeMap<String,Graph>();
-            graphs.put("e",s.trueModel.getGraphByName("e"));
-            graphs.put("r1",s.trueModel.getGraphByName("r2"));
-            Model synOnlyTrueModel = new Model(graphs,n);
-            tempRes = synOnlyProbModel.computeComparison(synOnlyTrueModel);
-            rval[0] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[1]);
-            rval[1] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[2]);
-            rval[2] = 2.0 * (double)tempRes[0] / (double)(2 * tempRes[0] + tempRes[1] + tempRes[2]);
-            graphs = new TreeMap<String,Graph>();
-            graphs.put("e",s.probModel.getGraphByName("e"));
-            graphs.put("r1",s.probModel.getGraphByName("r1"));
-            Model hypOnlyProbModel = new Model(graphs,n);
-            graphs = new TreeMap<String,Graph>();
-            graphs.put("e",s.trueModel.getGraphByName("e"));
-            graphs.put("r1",s.trueModel.getGraphByName("r1"));
-            Model hypOnlyTrueModel = new Model(graphs,n);
-            tempRes = hypOnlyProbModel.computeComparison(hypOnlyTrueModel);
-            rval[3] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[1]);
-            rval[4] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[2]);
-            rval[5] = 2.0 * (double)tempRes[0] / (double)(2 * tempRes[0] + tempRes[1] + tempRes[2]);
-            
-            ConsistSolver cs = new ConsistSolver();
-            cs.solve(new Logic(new File("logics/sh.logic")),s.probModel);
-            Model shSolvedModel = s.probModel.createSpecificCopy();
-            shSolvedModel.symmDiffAll(cs.soln);
-            
-            graphs = new TreeMap<String,Graph>();
-            graphs.put("e",shSolvedModel.getGraphByName("e"));
-            graphs.put("r1",shSolvedModel.getGraphByName("r2"));
-            Model shSynSolved = new Model(graphs,n);
-            tempRes = shSynSolved.computeComparison(synOnlyTrueModel);
-            rval[6] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[1]);
-            rval[7] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[2]);
-            rval[8] = 2.0 * (double)tempRes[0] / (double)(2 * tempRes[0] + tempRes[1] + tempRes[2]);
-            
-            graphs = new TreeMap<String,Graph>();
-            graphs.put("e",shSolvedModel.getGraphByName("e"));
-            graphs.put("r1",shSolvedModel.getGraphByName("r1"));
-            Model shHypSolved = new Model(graphs,n);
-            tempRes = shHypSolved.computeComparison(hypOnlyTrueModel);
-            rval[9] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[1]);
-            rval[10] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[2]);
-            rval[11] = 2.0 * (double)tempRes[0] / (double)(2 * tempRes[0] + tempRes[1] + tempRes[2]);
-            
-            cs = new ConsistSolver();
-            cs.solve(new Logic(new File("logics/synonym.logic")), synOnlyProbModel);
-            Model synSolvedModel = synOnlyProbModel.createSpecificCopy();
-            synSolvedModel.symmDiffAll(cs.soln);
-            tempRes = synSolvedModel.computeComparison(synOnlyTrueModel);
-            rval[12] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[1]);
-            rval[13] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[2]);
-            rval[14] = 2.0 * (double)tempRes[0] / (double)(2 * tempRes[0] + tempRes[1] + tempRes[2]);
-            
-            cs = new ConsistSolver();
-            cs.solve(new Logic(new File("logics/hypernym.logic")), hypOnlyProbModel);
-            Model hypSolvedModel = hypOnlyProbModel.createSpecificCopy();
-            hypSolvedModel.symmDiffAll(cs.soln);
-            tempRes = hypSolvedModel.computeComparison(hypOnlyTrueModel);
-            rval[15] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[1]);
-            rval[16] = (double)tempRes[0] / (double)(tempRes[0] + tempRes[2]);
-            rval[17] = 2.0 * (double)tempRes[0] / (double)(2 * tempRes[0] + tempRes[1] + tempRes[2]);
-            
-            for(int i = 0; i < 18; i += 3) {
-                System.out.println(rval[i] + " , " + rval[i+1] + " , " + rval[i+2]);
-            }
-            
-        } catch(Exception x) {
-            x.printStackTrace();
-            System.exit(-1);
-        }
-        return null;
-    }
     
 }
